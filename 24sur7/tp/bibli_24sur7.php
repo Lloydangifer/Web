269254424a925aacf6fbd5188a1a67f4b0006b2b
Lloydangifer <vmanrique@netcourrier.com>
2015-02-16 15:57:43 +0100

    maj

---
+++ 24sur7/tp/bibli_24sur7.php
@@ -0,0 +1,13 @@
+<?php
+vm_html_head($titre, $css = 'style.css'){
+echo'<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>',$titre,'</title>
+		<link rel="stylesheet" href="../styles/',$css,'" type="text/css">
+		<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon">
+	</head>
+}';
+
+?>
